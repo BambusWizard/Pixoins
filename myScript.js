13af325117ea593489fad4c06d5fdac73d2cd591
Bambus Wizardcock <bambuswizardcock@gmail.com>
2018-05-17 12:57:13 +0300

    Test again

--- myScript.js
+++ myScript.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 function startTime(){
   var today = new Date();
   alert(today);
 }
-=======
-function myFunction() {
-   document.getElementById("demo").innerHTML = "Paragraph changed.";
-}
->>>>>>> 4621d6223b77d054591919d5f4db5263ad2a51a6
